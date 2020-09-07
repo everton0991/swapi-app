741a4b7a9e7f1c85ee667f4f2949f2fa95750747
Everton Renan <everton.renan.pc@gmail.com>
2020-09-07 15:13:20 -0300

    feat: add folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,27 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
-import logo from './logo.svg';
-import './App.css';
+import * as S from './styles';
 
 function App() {
   return (
     <ThemeProvider theme={{}}>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-
+      <S.App>
+        <S.AppHeader>
           <p>
             Edit
             <code>src/App.tsx</code>
             and save to reload.
           </p>
-          <a
-            className="App-link"
+
+          <S.AppLink
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
           >
             Learn React
-          </a>
-        </header>
-      </div>
+          </S.AppLink>
+        </S.AppHeader>
+      </S.App>
     </ThemeProvider>
   );
 }

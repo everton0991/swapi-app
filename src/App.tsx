d310d082cbfb7102079f0c06ab73b58ae7f00e5e
Everton Renan <everton.renan.pc@gmail.com>
2020-09-07 13:23:58 -0300

    chore: add correct eslint configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import React from "react";
-import { ThemeProvider } from "styled-components";
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
@@ -10,7 +10,11 @@ function App() {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.tsx</code> and save to reload.
+            Edit
+            {' '}
+            <code>src/App.tsx</code>
+            {' '}
+            and save to reload.
           </p>
           <a
             className="App-link"

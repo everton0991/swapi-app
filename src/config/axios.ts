e56020ac90fda0de5f37557ebbd289eb4675f793
Everton Renan <everton.renan.pc@gmail.com>
2020-12-22 01:07:07 -0300

    chore: add metatag for CORS polocy

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -1,5 +1,8 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://swapi.dev/api/'
+  baseURL: 'https://swapi.dev/api/',
+  headers: {
+    'Content-Type': 'application/json'
+  }
 });
